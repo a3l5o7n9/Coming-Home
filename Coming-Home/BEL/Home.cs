bbc226a66d5380584795190cf87eba95a86fac50
a3l5o7n9 <39766138+a3l5o7n9@users.noreply.github.com>
2018-10-21 19:49:19 +0300

    -Added fields 'Latitude', 'Longitude', 'Altitude' and 'Accuracy' to class 'Home'.

--- Coming-Home/BEL/Home.cs
+++ Coming-Home/BEL/Home.cs
@@ -12,6 +12,10 @@ namespace BEL
         public string HomeName { get; set; }
         public int NumOfUsers { get; set; }
         public string Address { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Altitude { get; set; }
+        public string Accuracy { get; set; }
 
         public Home(int homeId, string homeName, int numOfUsers, string address)
         {
